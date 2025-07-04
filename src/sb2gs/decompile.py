f8b60945a66b438046d70ba0d37bcf6db1791336
aspizu <aspizu@protonmail.com>
2025-07-04 15:23:50 +0530

    refactor: decompile.py

--- src/sb2gs/decompile.py
+++ src/sb2gs/decompile.py
@@ -18,33 +18,32 @@ if TYPE_CHECKING:
 
 
 def decompile(input: Path, output: Path) -> None:
+    assets_path = output.joinpath("assets")
     shutil.rmtree(output, ignore_errors=True)
     output.mkdir(parents=True, exist_ok=True)
-    assets_path = output.joinpath("assets")
     with ZipFile(input) as zf, zf.open("project.json") as f:
         project = json.load(f, object_hook=JSONObject)
         for file in zf.filelist:
-            if file.filename == "project.json":
-                continue
-            zf.extract(file, assets_path)
+            if file.filename != "project.json":
+                zf.extract(file, assets_path)
+    stage = next(target for target in project.targets if target.isStage)
+    sprites = [target for target in project.targets if not target.isStage]
     if project.meta.semver != "3.0.0":
         msg = f"project semver ({project.meta.semver}) is unsupported"
         raise Error(msg)
     if project.meta.vm not in {"0.2.0", "11.3.0"}:
         msg = f"project vm version ({project.meta.vm}) is unsupported"
         raise Error(msg)
-    stage = next(target for target in project.targets if target.isStage)
-    sprites = [target for target in project.targets if not target.isStage]
+    ctx = Ctx(stage)
     with output.joinpath("stage.gs").open("w") as file:
-        ctx = Ctx(stage)
         decompile_sprite(ctx)
         file.write(str(ctx))
+    fixed = set()
     for target in sprites:
-        fixed = set()
+        ctx = Ctx(target)
         for costume in target.costumes:
             costumes.fix_center(costume, assets_path.joinpath(costume.md5ext), fixed)
         with output.joinpath(f"{target.name}.gs").open("w") as file:
-            ctx = Ctx(target)
             decompile_sprite(ctx)
             file.write(str(ctx))
     with output.joinpath("goboscript.toml").open("w") as f:
